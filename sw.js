d4a2d07a4e9ae5ebb2eef1be9c95e7cf647e2306
peshkovyu-boop <peshkov.yu@gmail.com>
2025-08-11 03:18:39 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calctracker-cache-v22';
+const CACHE_NAME = 'calctracker-cache-v25';
 const PRECACHE = [
   './',
   './index.html',
@@ -45,6 +45,7 @@ self.addEventListener('fetch', (e) => {
     })());
   }
 });
+
 
 
 
