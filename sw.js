b2664ecfb7ce669dcea12e8258c5af9e36c15bdd
peshkovyu-boop <peshkov.yu@gmail.com>
2025-08-16 13:41:15 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calctracker-cache-v33'; // ↑ меняй номер при каждом апдейте
+const CACHE_NAME = 'calctracker-cache-v34'; // ↑ меняй номер при каждом апдейте
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
@@ -43,3 +43,4 @@ self.addEventListener('fetch', (event) => {
     return cached || network || fetch(request);
   })());
 });
+
